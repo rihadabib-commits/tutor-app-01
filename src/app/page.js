5d344934d74363be26569b1a5103053bd41b2408
rihadabib-commits <rihadabib@gmail.com>
2026-05-21 22:55:46 +0600

    fixing

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,27 @@
+// import Abdul from "@/components/Abdul";
+// import Banner from "@/components/Banner";
+// import HeroBanner from "@/components/Banner";
+// import Navber from "@/components/Navber";
+// import Rihad from "@/components/Rihad";
+// import Wahid from "@/components/Wahid";
+// import { Card } from "@heroui/react";
+// import Image from "next/image";
+
+// export default function Home() {
+//   return (
+//    <div>
+//     {/* <Navber></Navber> */}
+//     <Banner/>
+//     <Wahid/>
+//     <Rihad/>
+//     <Abdul/>
+//    </div>
+//   );
+// }
+
+// এই ফাইলটিকে ডাইনামিক রেন্ডার করতে বাধ্য করার জন্য একদম উপরে এই লাইনটি দিন:
+export const dynamic = 'force-dynamic'; 
+
 import Abdul from "@/components/Abdul";
 import Banner from "@/components/Banner";
 import HeroBanner from "@/components/Banner";
@@ -10,7 +34,6 @@ import Image from "next/image";
 export default function Home() {
   return (
    <div>
-    {/* <Navber></Navber> */}
     <Banner/>
     <Wahid/>
     <Rihad/>
